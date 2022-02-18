02e1fd5805d059aed936b9e93d062f1b401e7baf
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-18 12:33:08 -0500

    added queries for Index/Show

---
+++ back-end/queries/products.js
@@ -0,0 +1,30 @@
+const db = require("../db/dbConfig");
+
+const getAllProducts = async () => {
+    try {
+        const allProducts = await db.any("SELECT * FROM products");
+        return allProducts;
+
+    } catch (err) {
+        throw err
+    }
+}
+
+const getOneProduct = async (id) => {
+    try {
+        const product = await db.one("SELECT * FROM products WHERE id=$1", id);
+        return product;
+    } catch (err) {
+        throw err
+    }
+}
+
+
+
+module.exports = {
+    getAllProducts,
+    getOneProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+}
