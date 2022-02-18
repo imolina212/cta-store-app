bb4609baef63ae2697cff3f913917c2b39239a73
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-18 17:48:17 -0500

    added seed data

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,11 @@
 \c cta_dev;
 
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO products (name, image, description, price, rating, featured)
+VALUES
+('Monday', 'https://picsum.photos/200', 'Great', 300, 3, false),
+('Tuesday', 'https://picsum.photos/200', 'Beautiful', 300, 4, false),
+('Wednesday', 'https://picsum.photos/200', 'Amazing', 300, 5, true),
+('Thursday', 'https://picsum.photos/200', 'Wow', 300, 4, true),
+('Friday', 'https://picsum.photos/200', 'Dope', 300, 3, false),
+('Saturday', 'https://picsum.photos/200', 'Fire', 300, 5, true),
+('Sunday', 'https://picsum.photos/200', 'Cool', 300, 4, false);
