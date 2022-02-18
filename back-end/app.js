b25f04ef3c94d35b8e78b76819d68b1c98cde13c
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-18 12:38:24 -0500

    added products controller dependencies + routes

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const cors = require("cors");
 const express = require("express");
+const productsController = require("./controllers/productsController")
 
 // CONFIGURATION
 const app = express();
@@ -9,24 +10,31 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Parse incoming JSON
 
+//CONTROLLER
+app.use("/products", productsController)
+
 // ROUTES
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Welcome to Isidro's Store!");
+});
+
+app.get("*",  (_, res) => {
+  res.status(404).send("404 Page Not Found")
 });
 
 /////////////////////////////////////
 // REMOVE AFTER SUCCESSFUL DEPLOYMENT
 /////////////////////////////////////
-const db = require("./db/dbConfig.js");
+// const db = require("./db/dbConfig.js");
 
-app.get("/test", async (req, res) => {
-  try {
-    const allDays = await db.any("SELECT * FROM test");
-    res.json(allDays);
-  } catch (err) {
-    res.json(err);
-  }
-});
+// app.get("/test", async (req, res) => {
+//   try {
+//     const allDays = await db.any("SELECT * FROM test");
+//     res.json(allDays);
+//   } catch (err) {
+//     res.json(err);
+//   }
+// });
 
 /////////////////////////////////////
 // REMOVE AFTER SUCCESSFUL DEPLOYMENT
