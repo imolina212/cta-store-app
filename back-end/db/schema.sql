5e050336a145a414d0e0e34a82a4b476b767d1cf
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-18 17:47:44 -0500

    added schema

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -3,9 +3,14 @@ CREATE DATABASE cta_dev;
 
 \c cta_dev;
 
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS products;
 
-CREATE TABLE test (
+CREATE TABLE products (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    name TEXT NOT NULL,
+    image TEXT,
+    description TEXT,
+    price INT,
+    rating INT,
+    featured BOOLEAN
 );
