8b1e0f619818962ed01716b464195dd1c3d4fd58
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-20 13:41:54 -0500

    added app dependencies/routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,53 @@
 import axios from "axios";
+//DEPENDENCIES
 import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 const API = process.env.REACT_APP_API_URL;
 
-console.log(API);
+
+//COMPONENTS
+import NavBar from "./Components/NavBar"
+
+//PAGES
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import Show from "./Pages/Show";
+import New from "./Pages/New";
+import Edit from "./Pages/Edit";
+import Error from "./Pages/Error";
+
 function App() {
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => {
-          setDays(response.data);
-        },
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
+  <>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/products" element={<Index />}/>
+        <Route path="/products/:id" element={<Show />}/>
+        <Route path="/products/new" element={<New />}/>
+        <Route path="/products/:id/edit" element={<Edit />}/>
+        <Route path="*" element={<Error />}/>
+      </Routes>
+    </>
+
+  // const [days, setDays] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get(`${API}/test`)
+  //     .then(
+  //       (response) => {
+  //         setDays(response.data);
+  //       },
+  //       (error) => console.log("get", error)
+  //     )
+  //     .catch((c) => console.warn("catch", c));
+  // }, []);
   return (
     <div>
-      <ul>
+      {/* <ul>
         {days.map((day) => (
           <li key={day.name}>{day.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
