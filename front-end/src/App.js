6fa0303f2ddcb6da9968f69af637eee8ee3b6673
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-20 14:35:31 -0500

    components now rendering

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,5 @@
-import axios from "axios";
 //DEPENDENCIES
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-const API = process.env.REACT_APP_API_URL;
-
 
 //COMPONENTS
 import NavBar from "./Components/NavBar"
@@ -17,39 +13,18 @@ import Edit from "./Pages/Edit";
 import Error from "./Pages/Error";
 
 function App() {
-  <>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route path="/products" element={<Index />}/>
-        <Route path="/products/:id" element={<Show />}/>
-        <Route path="/products/new" element={<New />}/>
-        <Route path="/products/:id/edit" element={<Edit />}/>
-        <Route path="*" element={<Error />}/>
-      </Routes>
-    </>
-
-  // const [days, setDays] = useState([]);
-  // useEffect(() => {
-  //   axios
-  //     .get(`${API}/test`)
-  //     .then(
-  //       (response) => {
-  //         setDays(response.data);
-  //       },
-  //       (error) => console.log("get", error)
-  //     )
-  //     .catch((c) => console.warn("catch", c));
-  // }, []);
   return (
-    <div>
-      {/* <ul>
-        {days.map((day) => (
-          <li key={day.name}>{day.name}</li>
-        ))}
-      </ul> */}
-    </div>
+    <>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/products" element={<Index />}/>
+          {/* <Route path="/products/:id" element={<Show />}/>
+          <Route path="/products/new" element={<New />}/>
+          <Route path="/products/:id/edit" element={<Edit />}/>
+          <Route path="*" element={<Error />}/> */}
+        </Routes>
+      </>
   );
 }
-
 export default App;
