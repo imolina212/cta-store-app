b0bcf442a865ba616786944b4d0c9ffefe1618ce
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-23 00:32:26 -0500

    refactored w/bootstrap

--- front-end/src/Components/Product.js
+++ front-end/src/Components/Product.js
@@ -1,12 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Button, Card } from "react-bootstrap"
 
 function Product({product}) {
   return (
-    <div className="Product">
-      <Link to={`/products/${product.id}`}>
-        <img src={product.image} alt="products" />
-      </Link>
-    </div>
+      <Card className="Product">
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+          </Card.Text>
+          <Link to={`/products/${product.id}`}>
+            <Card.Img className="img-fluid" src={product.image} alt="products" />
+          </Link>
+          <Button className="my-4" variant="outline-primary">Add to Cart</Button>
+        </Card.Body>
+      </Card>
   )
 }
 
