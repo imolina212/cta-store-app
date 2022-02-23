98b81dc0fb218b5c7ba31ca9e98708a622020ac7
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-23 17:17:09 -0500

    created Navbar

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,11 +1,28 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap"
 
 function NavBar() {
   return (
-    <div className="NavBar">
-        <h2>NavBar</h2>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">CTA-Store App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/products">Collections</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
-export default NavBar
+export default NavBar;
