1cf1ccdf638501e848f8a53e5ac1eaa3ea325324
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-23 21:28:39 -0500

    modified seed data

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -3,9 +3,9 @@
 INSERT INTO products (name, image, description, price, rating, featured)
 VALUES
 ('Monday', 'https://picsum.photos/200', 'Great', 300, 3, false),
-('Tuesday', 'https://picsum.photos/201', 'Beautiful', 300, 4, false),
-('Wednesday', 'https://picsum.photos/202', 'Amazing', 300, 5, true),
-('Thursday', 'https://picsum.photos/203', 'Wow', 300, 4, true),
-('Friday', 'https://picsum.photos/204', 'Dope', 300, 3, false),
-('Saturday', 'https://picsum.photos/205', 'Fire', 300, 5, true),
-('Sunday', 'https://picsum.photos/206', 'Cool', 300, 4, false);
+('Tuesday', 'https://picsum.photos/201', 'Beautiful', 634, 4, false),
+('Wednesday', 'https://picsum.photos/202', 'Amazing', 930, 5, true),
+('Thursday', 'https://picsum.photos/203', 'Wow', 280, 4, true),
+('Friday', 'https://picsum.photos/204', 'Dope', 790, 3, false),
+('Saturday', 'https://picsum.photos/205', 'Fire', 1270, 5, true),
+('Sunday', 'https://picsum.photos/206', 'Cool', 279, 4, false);
