37046c635bc5eaff81b3ea5e4f941f07758ab637
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-23 21:06:55 -0500

    linked buttons

--- front-end/src/Components/ProductDetails.js
+++ front-end/src/Components/ProductDetails.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { Card, Button, Stack } from "react-bootstrap"
+import { Card, Button, Stack , Container } from "react-bootstrap"
 import { useState, useEffect } from "react";
 import { useParams , useNavigate, Link} from "react-router-dom";
 
@@ -23,22 +23,25 @@ function ProductDetails() {
 
   
   return (
-    <Card>
-        <Card.Body>
-          <Card.Title>{product.name}</Card.Title>
-          <Link to={`/products/${product.id}`}>
-            <Card.Img className="img-fluid" src={product.image} alt="products" />
-          </Link>
-          <Card.Text className="mt-3">
-          Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-          </Card.Text>
-          <Stack direction="horizontal" gap="3">
-            <Button className="my-4" variant="outline-danger" onClick={deleteProduct}>Delete</Button>
-            <Button className="my-4" variant="outline-primary">Add to Cart</Button>
-          </Stack>
-        </Card.Body>
-      </Card>
+    <Container className="mt-5 p-5">
+      <Card>
+          <Card.Body>
+            <Card.Title>{product.name}</Card.Title>
+            <Link to={`/products/${product.id}`}>
+              <Card.Img className="p-3" src={product.image} alt="products" />
+            </Link>
+            <Card.Text className="mt-3">
+            Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+            </Card.Text>
+            <Stack direction="horizontal" gap="3">
+              <Button className="my-4" variant="outline-danger" onClick={deleteProduct}>Delete</Button>
+              <Button href={`/products/${id}/edit`} className="my-4" variant="outline-primary">Edit</Button>
+              <Button className="my-4" variant="outline-primary">Add to Cart</Button>
+            </Stack>
+          </Card.Body>
+        </Card>
+    </Container>
     
   )
 }
