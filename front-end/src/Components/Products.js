d70edb7556da870b69f3731a365db7ddfeb486c8
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-24 23:04:59 -0500

    added onclick to add to cart button

--- front-end/src/Components/Products.js
+++ front-end/src/Components/Products.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Product from "./Product";
-import { Stack } from "react-bootstrap"
+import { Stack , Button } from "react-bootstrap"
+import Cart from "../Components/Cart"
 
 function Products() {
   const [products, setProducts] = useState([]);
-  const URL = process.env.REACT_APP_API_URL
+  const [cart, setCart] = useState([]);
+  const [subtotal, setSubtotal] = useState([]);
+  const URL = process.env.REACT_APP_API_URL;
 
   useEffect(()=> {
     axios.get(`${URL}/products`)
@@ -18,15 +21,29 @@ function Products() {
       })
   },[URL])
 
+  const handleAddToCart = (product) => {
+    setCart([...cart, product]);
+    setSubtotal(subtotal + product.price);
+  }
+  
+  
   return (
     <main className='Products'>
       <section>
         <Stack direction="vertical" gap="5" style={{display: "grid", gridTemplateColumns: "1fr 1fr", textAlign: "center"}}>
           {products.map((product) => {
-            return <Product key={product.id} product={product}/>
+            return(
+              <div>
+                <Product key={product.id} product={product}/>
+                <Button className="my-3" onClick={()=>{handleAddToCart(product)}}>Add to cart</Button>
+              </div>
+            )
           })}
         </Stack>
       </section>
+        <aside>
+          <Cart cart={cart} subtotal={subtotal} />
+        </aside>
     </main>
   )
 }
