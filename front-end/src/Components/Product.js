0edaad1afb85793de78755bb4ce837806378e6ba
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-02-24 23:09:02 -0500

    updated card

--- front-end/src/Components/Product.js
+++ front-end/src/Components/Product.js
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-import { Button, Card } from "react-bootstrap"
+import { Card } from "react-bootstrap"
+import "./Product.css"
 
 function Product({product}) {
   return (
-      <Card>
+      <Card id="card">
         <Card.Body>
           <Link to={`/products/${product.id}`}>
-            <Card.Img style={{width: '250px'}} src={product.image} alt="products" />
+            <Card.Img style={{width: '100%'}} src={product.image} alt="products" />
           </Link>
           <Card.Title className="my-2">{product.name}</Card.Title>
           <Card.Text className="me-auto">${product.price.toFixed(2)}</Card.Text>
         </Card.Body>
-        <Button className="me-auto m-3" variant="outline-primary">Add to Cart</Button>
       </Card>
   )
 }
